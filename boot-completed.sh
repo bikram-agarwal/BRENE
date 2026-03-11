c5c9cc5795d4cfb5edde8500623b4c78d9dd6953
Rene <rrr333nnn333@gmail.com>
2026-03-11 18:40:38 -0400

    improve: move prop spoofing to late_start service mode (service.sh)

--- boot-completed.sh
+++ boot-completed.sh
@@ -108,53 +108,6 @@ if [[ $config_hide_data_local_tmp == 1 ]]; then
 fi
 
 
-## Props ##
-resetprop_n "init.svc.adbd" "stopped"
-resetprop_n "init.svc_debug_pid.adbd" ""
-resetprop_n "persist.sys.usb.config" "mtp"
-resetprop_n "sys.oem_unlock_allowed" "0"
-resetprop_n "ro.adb.secure" "1"
-resetprop_n "ro.crypto.state" "encrypted"
-resetprop_n "ro.debuggable" "0"
-resetprop_n "ro.force.debuggable" "0"
-resetprop_n "ro.kernel.qemu" ""
-resetprop_n "ro.secure" "1"
-resetprop_n "ro.build.selinux" "1"
-resetprop_n "ro.build.selinux.enforce" "1"
-resetprop_n "ro.secureboot.lockstate" "locked"
-resetprop_n "ro.is_ever_orange" "0"
-resetprop_n "ro.bootmode" "normal"
-resetprop_n "ro.bootimage.build.tags" "release-keys"
-
-resetprop_n "ro.build.type" "user"
-resetprop_n "ro.build.tags" "release-keys"
-
-resetprop_n "vendor.boot.vbmeta.device_state" "locked"
-resetprop_n "vendor.boot.verifiedbootstate" "green"
-
-resetprop_n "ro.boot.flash.locked" "1"
-resetprop_n "ro.boot.realme.lockstate" "1"
-resetprop_n "ro.boot.realmebootstate" "green"
-resetprop_n "ro.boot.verifiedbooterror" ""
-resetprop_n "ro.boot.verifiedbootstate" "green"
-resetprop_n "ro.boot.veritymode" "enforcing"
-resetprop_n "ro.boot.veritymode.managed" "yes"
-
-resetprop_n "ro.boot.vbmeta.size" "4096"
-resetprop_n "ro.boot.vbmeta.hash_alg" "sha256"
-resetprop_n "ro.boot.vbmeta.avb_version" "1.3"
-resetprop_n "ro.boot.vbmeta.device_state" "locked"
-resetprop_n "ro.boot.vbmeta.invalidate_on_error" "yes"
-
-if_prop_value_exits_resetprop_n "ro.warranty_bit" "0"
-if_prop_value_exits_resetprop_n "ro.vendor.boot.warranty_bit" "0"
-if_prop_value_exits_resetprop_n "ro.vendor.warranty_bit" "0"
-if_prop_value_exits_resetprop_n "ro.boot.warranty_bit" "0"
-
-fingerprint=$(resetprop ro.build.fingerprint)
-resetprop_n "ro.build.fingerprint" "${fingerprint//userdebug/user}"
-
-
 ## Please note that sometimes the path needs to be added twice or above to be effective ##
 ## Besides, all user apps without root access cannot see the hidden path '/sdcard/<hidden_path>' unless you grant it root access ##
 ## First we need to wait until files are accessible in /sdcard ##
