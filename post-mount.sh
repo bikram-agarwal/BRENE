078dc6097d1b3b4df2ed2904e58a1e871437c2f4
Rene <rrr333nnn333@gmail.com>
2026-04-11 21:08:35 -0400

    refactor: post-mount.sh

--- post-mount.sh
+++ post-mount.sh
@@ -1,11 +1,17 @@
 # MODDIR=${0%/*}
-# DEST_BIN_DIR=/data/adb/ksu/bin
-# KSU_BIN=/data/adb/ksu/bin/ksud
+# KSU_BIN=/data/adb/ksud
+# KSU_MODULES_DIR=/data/adb/modules
 # SUSFS_BIN=/data/adb/ksu/bin/susfs
 # PERSISTENT_DIR=/data/adb/brene
+# DEST_BIN_DIR=/data/adb/ksu/bin
+
+
+
+# # Load utils
+# [[ -e "${MODDIR}/utils.sh" ]] && source "${MODDIR}/utils.sh"
 # # Load config
-# [[ -e ${PERSISTENT_DIR}/config.sh ]] && source ${PERSISTENT_DIR}/config.sh
-# source ${MODDIR}/utils.sh
+# [[ -e "${PERSISTENT_DIR}/config.sh" ]] && source "${PERSISTENT_DIR}/config.sh"
+
+
 
 # echo "EOF" >> "${PERSISTENT_DIR}/log.txt"
-# # EOF
